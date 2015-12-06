3bf1fa64972b7387a80a4d24502f73100f51bd59
mkukula <marta.kukula.mk@gmail.com>
2015-12-06 02:32:07 +0100

    route and templates

--- js/script.js
+++ js/script.js
@@ -1,38 +1,73 @@
-var app = angular.module("myApp", ['myFilters']);
+(function(angular) {
+  'use strict';
+angular.module('ngRouteExample', ['ngRoute', 'myFilters'])
 
-app.controller("myController", function($scope, $http) {
+ .controller('MainController', function($scope, $route, $routeParams, $location) {
+     $scope.$route = $route;
+     $scope.$location = $location;
+     $scope.$routeParams = $routeParams;
+ })
 
-	$scope.currentView = "postListView";
+ .controller('PostController', function($scope, $routeParams, $http) {
+     $scope.currentView = "postListView";
+     $scope.name = "PostController";
+     $scope.params = $routeParams;
+     $scope.currentView = "postListView";
+     $scope.posts = [];
+     $http.get("http://jsonplaceholder.typicode.com/posts").success(function(data) {
+      $scope.posts = data;
+     });
+     $scope.users = [];
+     $http.get("http://jsonplaceholder.typicode.com/users").success(function(data) {
+      $scope.users = data;
+     });
+     $scope.goToPost = function(id) {
+      $http.get("http://jsonplaceholder.typicode.com/posts/" + id).success(function(data) {
+      $scope.currentPost = data;
+      $scope.currentView = "postView";
+      });
+     }       
+ })
 
-	$scope.posts = [];
-	$http.get("http://jsonplaceholder.typicode.com/posts").success(function(data) {
-		$scope.posts = data;
-	});
+ .controller('UserController', function($scope, $routeParams, $http) {
+     $scope.currentView = "userListView";
+     $scope.name = "UserController";
+     $scope.params = $routeParams;
+     $scope.posts = [];
+     $http.get("http://jsonplaceholder.typicode.com/posts").success(function(data) {
+      $scope.posts = data;
+     });
+     $scope.users = [];
+     $http.get("http://jsonplaceholder.typicode.com/users").success(function(data) {
+      $scope.users = data;
+     }); 
+     $scope.listUserPosts = function(userId) {
+      $http.get("http://jsonplaceholder.typicode.com/posts?userId=" + userId).success(function(data) {
+      $scope.userPosts = data;
+      $scope.currentView = "userPostsView";
+     });
+     };
+ })
 
-	$scope.users = [];
-	$http.get("http://jsonplaceholder.typicode.com/users").success(function(data) {
-		$scope.users = data;
-	});
+.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl: 'posty.html',
+    controller: 'PostController',
+  })
+  .when('/Posty', {
+    templateUrl: 'posty.html',
+    controller: 'PostController',
+  })
+  .when('/Uzytkownicy', {
+    templateUrl: 'uzytkownicy.html',
+    controller: 'UserController',
+  })
+  .when('/OMnie', {
+    templateUrl: 'omnie.html',
+  });
 
-	$scope.currentPost = {};
-	
-	$scope.goToPost = function(id) {
-		$http.get("http://jsonplaceholder.typicode.com/posts/" + id).success(function(data) {
-			$scope.currentPost = data;
-			$scope.currentView = "postView";
-		});
-	};
-
-	$scope.changeView = function(viewName) {
-		$scope.currentView = viewName;
-	};
-
-	$scope.userPosts = [];
-	$scope.listUserPosts = function(userId) {
-		$http.get("http://jsonplaceholder.typicode.com/posts?userId=" + userId).success(function(data) {
-			$scope.userPosts = data;
-			$scope.currentView = "userPostsView";
-		});
-	};
+  // configure html5 to get links working on jsfiddle
+  $locationProvider.html5Mode(true);
 });
-
+})(window.angular);
